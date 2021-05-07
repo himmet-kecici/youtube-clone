9ea5ce514d6bfb01cb7b4bc7af906566c42e70e3
himmet <kecicihimmet@gmail.com>
2021-05-08 01:39:16 +0300

    set sidebar.scss

--- src/components/sidebar/sidebar.jsx
+++ src/components/sidebar/sidebar.jsx
@@ -1,11 +1,47 @@
 import React from 'react'
 import './sidebar.scss'
+import {
+    MdSubscriptions,
+    MdExitToApp,
+    MdThumbUp,
+    MdHistory,
+    MdLibraryBooks,
+    MdHome
+} from 'react-icons/md'
 
 const Sidebar = () => {
     return (
-        <div className="border border-danger">
-            sidebar
-        </div>
+        <nav className="border border-danger sidebar">
+            <li>
+                <MdHome size={23} />
+                <span>Home</span>
+            </li>
+            <li>
+                <MdSubscriptions size={23} />
+                <span>Subscriptions</span>
+            </li>
+            <li>
+                <MdThumbUp size={23} />
+                <span>liked</span>
+            </li>
+
+            <li>
+                <MdHistory size={23} />
+                <span>History</span>
+            </li>
+            <li>
+                <MdLibraryBooks size={23} />
+                <span>Library</span>
+            </li>
+
+            <hr />
+            <li>
+                <MdExitToApp size={23} />
+                <span>log out</span>
+            </li>
+
+            <hr />
+        </nav>
     )
 }
 
