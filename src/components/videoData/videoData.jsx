772ab036e27267820866bb543a571f05fd3d8f6d
himmet <kecicihimmet@gmail.com>
2021-10-22 16:25:25 +0300

    added some css and react-show-more-text library

--- src/components/videoData/videoData.jsx
+++ src/components/videoData/videoData.jsx
@@ -3,6 +3,8 @@ import './videoData.scss'
 import moment from 'moment'
 import numeral from 'numeral'
 import { MdThumbUp, MdThumbDown } from 'react-icons/md'
+import ShowMoreText from 'react-show-more-text'
+
 
 
 const VideoData = () => {
@@ -15,14 +17,15 @@ const VideoData = () => {
                         {numeral(10000).format('0.a')} Views •
                         {moment('2021-10-10').fromNow()}
                     </span>
-                </div>
-                <div>
-                    <span>
-                        <MdThumbUp size={26} />{numeral(10000).format('0.a')}
-                    </span>
-                    <span>
-                        <MdThumbDown size={26} />{numeral(10000).format('0.a')}
-                    </span>
+
+                    <div>
+                        <span className='mr-3'>
+                            <MdThumbUp size={26} />{numeral(10000).format('0.a')}
+                        </span>
+                        <span className='mr-3'>
+                            <MdThumbDown size={26} />{numeral(10000).format('0.a')}
+                        </span>
+                    </div>
                 </div>
             </div>
             <div className='video-data-channel d-flex justify-content-between align-items-center my-2 py-3'>
@@ -37,9 +40,21 @@ const VideoData = () => {
                 </div>
                 <button className='btn border-0 p-2 m-2'>Subscribe</button>
             </div>
-            <div className='video-data-description'></div>
+            <div className='video-data-description'>
+                <ShowMoreText
+                    lines={3}
+                    more='SHOW MORE'
+                    less='SHOW LESS'
+                    anchorClass='show-more-text'
+                    expanded={false}
+                >
+                    Lorem ipsum dolor sit amet. Qui magni sunt ut esse repudiandae non voluptatibus voluptas ab consequatur voluptatem qui velit vero sit quas dolores ut numquam perferendis. In sint consequatur sit cumque nihil et praesentium molestias sed dolorem nisi. Aut quibusdam quaerat cum iusto autem et laboriosam iusto ad tempore quasi. Eos temporibus voluptatum ut internos voluptas vel earum corrupti et neque voluptate in aspernatur tenetur et nobis quidem non reiciendis culpa
+                    Lorem ipsum dolor sit amet. Qui magni sunt ut esse repudiandae non voluptatibus voluptas ab consequatur voluptatem qui velit vero sit quas dolores ut numquam perferendis. In sint consequatur sit cumque nihil et praesentium molestias sed dolorem nisi. Aut quibusdam quaerat cum iusto autem et laboriosam iusto ad tempore quasi. Eos temporibus voluptatum ut internos voluptas vel earum corrupti et neque voluptate in aspernatur tenetur et nobis quidem non reiciendis culpa
 
-        </div>
+                </ShowMoreText>
+            </div>
+
+        </div >
     )
 }
 
