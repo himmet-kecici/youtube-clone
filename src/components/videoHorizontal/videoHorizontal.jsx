1a438c5c7393b16fcac67dd1db659de3bacc5ee4
himmet <kecicihimmet@gmail.com>
2021-11-22 18:38:51 +0300

    changed some css properties, created onclick event and used useHistory, added skeletontheme for watchingpage

--- src/components/videoHorizontal/videoHorizontal.jsx
+++ src/components/videoHorizontal/videoHorizontal.jsx
@@ -5,6 +5,7 @@ import moment from 'moment'
 import numeral from 'numeral'
 import { LazyLoadImage } from 'react-lazy-load-image-component'
 import { Col, Row } from 'react-bootstrap'
+import { useHistory } from 'react-router'
 
 
 const VideoHorizontal = ({ video }) => {
@@ -42,16 +43,20 @@ const VideoHorizontal = ({ video }) => {
     const seconds = moment.duration(duration).asSeconds()
     const _duration = moment.utc(seconds * 1000).format('mm:ss')
 
+    const history = useHistory()
+    const handleClick = () => {
+        history.push(`/watch${id.videoId}`)
+    }
     return (
-        <Row className="videoHorizontal m-1 py-1 align-items-center">
-            <Col xs={6} md={4} className='videoHorizontal-left'>
+        <Row className="videoHorizontal m-1 py-1 align-items-center" onClick={handleClick}>
+            <Col xs={6} md={6} className='videoHorizontal-left'>
                 <LazyLoadImage src={medium.url}
                     effect='blur'
                     className='videoHorizontal-thumbnail'
                     wrapperClassName='videoHorizontal-thumbnail-wrapper' />
-                <span className='video-top-duration'>{_duration}</span>
+                <span className='videoHorizontal-duration'>{_duration}</span>
             </Col>
-            <Col xs={6} md={8} className='videoHorizontal-right p-0'>
+            <Col xs={6} md={6} className='videoHorizontal-right p-0'>
                 <p className='videoHorizontal-title mb-1'> {title}</p>
                 <div className='videoHorizontal-details'>
 
@@ -60,7 +65,7 @@ const VideoHorizontal = ({ video }) => {
                     {moment(publishedAt).fromNow()}
                 </div>
                 <div className='videoHorizontal-channel d-flex align-item-center my-1'>
-                    <p>{channelTitle}</p>
+                    <p className='mb-0'>{channelTitle}</p>
                 </div>
             </Col>
         </Row >
--- src/pages/watchingPage/watchingPage.jsx
+++ src/pages/watchingPage/watchingPage.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect } from 'react'
 import { Col, Row } from 'react-bootstrap'
+import Skeleton, { SkeletonTheme } from 'react-loading-skeleton'
 import { useDispatch, useSelector } from 'react-redux'
 import { useParams } from 'react-router-dom'
 import Comments from '../../components/comments/comments'
@@ -50,7 +51,11 @@ const WatchingPage = () => {
 
             {
 
-                !loading && videos?.filter(video => video.snippet).map((video) => <VideoHorizontal video={video} key={video.id.videoId} />)
+                !loading ? videos?.filter(video => video.snippet).map((video) => <VideoHorizontal video={video} key={video.id.videoId} />) :
+
+                    <SkeletonTheme color='#343a40' highlightColor='#3c4147'>
+                        <Skeleton width="100" height="130px" count={15} />
+                    </SkeletonTheme>
             }
         </Col>
     </Row >
